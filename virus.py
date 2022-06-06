4ec565c8104181e810ec283f6cffc58c1df677d1
Drako9159 <chochito9159@gmail.com>
2022-06-06 02:00:48 -0500

    add turtle library

---
+++ virus.py
@@ -0,0 +1,15 @@
+# Create prisma
+# import turtle for drawn
+from turtle import *
+speed(10)
+color('green')
+bgcolor('black')
+
+number_repet = 200
+
+while number_repet > 0 :
+    left(number_repet)
+    forward(number_repet * 1)
+    #number_repet = number_repet + 1
+
+

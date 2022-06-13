72e304e2277d2c0c077772ccb48d75de2a24886c
Drako9159 <chochito9159@gmail.com>
2022-06-13 12:57:14 -0500

    new automatizations

---
+++ automatizaciones.py
@@ -0,0 +1,12 @@
+# Abrir archivo
+import os
+direction = "C:/Users/ANTONIO/Desktop/Homedale.exe"
+os.system(direction)
+
+## Mover mouse
+
+#import mouse
+#mouse.move(x = 80, y = 100, absolute=True, duration=2)
+#mouse.click(button='left')
+#mouse.drag(start_x=200, start_y=250, end_x=250, end_y=260, absolute=True, )
+

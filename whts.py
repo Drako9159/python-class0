cfd75c1adfc69a68efd488654dfe6cf0beda412f
drako9159 <chochito9159@gmail.com>
2022-07-16 01:09:32 -0500

    complete simple evalute sentence python

--- whts.py
+++ whts.py
@@ -1,12 +1,14 @@
-clave = "makinola"
+
+## Evaluate user password program
+password = input("Digite una contraseña :")
+evaluate = input("Ahora debe introducir su contraseña :")
 i = 1
-claveusr = input("Ingrese su contraseña")
-while claveusr != clave:
+while password != evaluate:
     if i < 3:
         i += 1
-    else:
-        print("Ha alcanzado el número máximo de intentos.")
-    claveusr = input((f"Contraseña incorrecta. Intente nuevamente ({i}/3 intentos): "))
-print("Contraseña correcta. Puede continuar.")
-
-
+        evaluate = input((f"Contraseña incorrecta. Intentelo de nuevo ({i}/3 intentos restantes): "))
+    else :
+        print("Se excedio el máximo de intentos")
+        break
+if password == evaluate:
+    print("La contraseña es correcta, puede continuar...")
